f690c102f898650b3f86a6cb1d58f7de3824c3a3
Harvey Siy <hsiy@unomaha.edu>
2020-06-18 13:17:15 -0500

    develop feature X

--- MainApp.java
+++ MainApp.java
@@ -2,6 +2,7 @@
 class MainApp {
 	public static void main(String args[]) {
 		MyClass mc = new MyClass("Bob", "Bob is a new student");
+		MyXClass mxc = new MyXClass("Alice", "Alice is a new student");
 	}
 }
 
